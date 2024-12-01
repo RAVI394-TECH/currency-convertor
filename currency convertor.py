9477335c2a3eccc08ddd74d45b3330b438a18eac
RAVI394-TECH <raviprataps737@gmail.com>
2024-12-01 15:21:43 +0530

    Add files via upload
    The Python function will return the currency conversion rate and the total converted amount. Here, the url, f"{base_url}? api_key={api_key}&from={from_currency}&to={to_currency}&amount={amount}", is formed by combining base URL and API key and the three params we mentioned above

---
+++ currency convertor.py
@@ -0,0 +1,10 @@
+from currency_converter import CurrencyConverter
+a=CurrencyConverter()
+
+print(a.convert(93,"INR","USD"))
+
+import  tkinter as tk
+window=tk.Tk
+window.geometry=("800*600")
+window.mainloop()
+window()
